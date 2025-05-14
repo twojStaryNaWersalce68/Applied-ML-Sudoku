11246cb7c421764603a84cd0ee976ddbd9109990
Tabea Jäkel <tabeajae@gmail.com>
2025-05-14 17:57:22 +0200

    Converts all images to RGB and greyscale

--- project_name/models/preprocessing.py
+++ project_name/models/preprocessing.py
@@ -16,11 +16,31 @@ def load_data(path):
 
 def show_image(image):
     """Shows an image from the dataset."""
-    plt.imshow(image)
+    plt.imshow(image, cmap="gray")
     plt.show()
 
 
+def convert_to_grayscale(ds):
+    """Converts the image to a rgb type and then to grayscale."""
+    rgb_image = ds['image'].convert('RGB')
+    grayscale_image = ImageOps.grayscale(rgb_image)
+    ds['image'] = grayscale_image
+    return ds
+
+
+def preprocess_dataset(ds_dict):
+    """Preprocesses the dataset."""
+    for split in ds_dict:
+        print(split)
+        ds_dict[split] = ds_dict[split].map(convert_to_grayscale)
+        show_image(dataset_dict[split][0]["image"])
+        print(dataset_dict[split][0]["keypoints"])
+
+
 if __name__ == '__main__':
-    ds_dict = load_data("Lexski/sudoku-image-recognition")
-    show_image(ds_dict["train"][0]["image"])
+    dataset_dict = load_data("Lexski/sudoku-image-recognition")
+    example_image = dataset_dict["train"][0]["image"]
+    show_image(example_image)
+    print(type(example_image))
+    preprocess_dataset(dataset_dict)
 
