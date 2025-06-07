d9a29ee2a0c3ef2bcf0dfc05bdd91a459d06cf62
Luca <luca.drouillet@gmail.com>
2025-06-07 14:32:42 +0200

    minor changes on feedback

--- sudoku_digitalisation/models/CNN.py
+++ sudoku_digitalisation/models/CNN.py
@@ -6,6 +6,7 @@ from PIL import Image
 from typing import Tuple, List, Union
 from sklearn.metrics import confusion_matrix, classification_report, ConfusionMatrixDisplay
 
+CELL_NUM = 81
 
 class CNN:
     def __init__(
@@ -150,15 +151,15 @@ class CNN:
         print(report)
 
         # accuracy on sudokus
-        num_sudokus = len(y_test)//81
+        num_sudokus = len(y_test)//CELL_NUM
         correct_sudokus = 0
         for i in range(num_sudokus):
-            start = i * 81
-            end = start + 81
+            start = i * CELL_NUM
+            end = start + CELL_NUM
             if np.array_equal(y_pred[start:end], y_test[start:end]):
                 correct_sudokus += 1
         correct_percent = correct_sudokus/num_sudokus
-        print(f"percentage of correct sudokus: {correct_percent}")
+        print(f"accuracy of fully correct sudokus: {correct_percent}")
 
         # Accuracy plot over time
         plt.plot(self.history.history['accuracy'], label='Training Accuracy')
