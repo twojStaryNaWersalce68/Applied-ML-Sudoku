ed6ac3615df04d64a458117aaf157b4c4639820d
divan <hegeman.ivan@gmail.com>
2025-06-09 17:36:41 +0200

    typing fix

--- sudoku_digitalisation/features/cell_splitter.py
+++ sudoku_digitalisation/features/cell_splitter.py
@@ -2,6 +2,7 @@ import numpy as np
 from PIL import Image
 from typing import List, Dict, Any
 import cv2
+from typing import Tuple
 from markdown.preprocessors import Preprocessor
 from tensorflow.python.ops.gen_array_ops import lower_bound
 
@@ -19,7 +20,7 @@ class CellSplitter:
         return candidate_digits
 
     @staticmethod
-    def determine_mode(image: np.array) -> (List[int], int):
+    def determine_mode(image: np.array) -> Tuple[List[int], int]:
         """
         Determines the mean colour of the pixels in the cell and returns this,
         as well as the binary threshold setting, which depends on whether the cell is
